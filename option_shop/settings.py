1b1d44f938152ff2ff62b9f426695368361e6b30
Dayveed <dayveed.d@gmail.com>
2024-02-08 07:51:03 -0800

    work on carts and test

--- option_shop/settings.py
+++ option_shop/settings.py
@@ -79,15 +79,6 @@ WSGI_APPLICATION = 'option_shop.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 DATABASES = {
 
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': 'djangodb',
-    #     'USER': 'djlocal',
-    #     'PASSWORD': 'djangopasswd',
-    #     'HOST': 'localhost',
-    #     'PORT': '3306',
-    # },
-
     #     'default': {
     #         'ENGINE': 'django.db.backends.sqlite3',
     #         'NAME': BASE_DIR / 'db.sqlite3',
